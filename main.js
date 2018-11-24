733b5f33786ec567e191c2877959b07ab2f18e40
George McCarron <hello@georgemccarron.com>
2018-11-24 02:27:24 +0000

    added functions to access mouse click grid coordinates

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const canvas = document.createElement('canvas');
-canvas.width = canvas.height = 520;
+const margin = 10;
+const cellSize = 50;
+const gridSize = 10;
+const nodes = [gridSize];
 
+canvas.width = canvas.height = margin * 2 + cellSize * gridSize;
 document.body.appendChild(canvas);
 
 const ctx = canvas.getContext('2d');
@@ -8,8 +12,25 @@ const ctx = canvas.getContext('2d');
 ctx.strokeStyle = '#000000';
 ctx.lineWidth = 2;
 
-for (let i = 10; i < 500; i += 50) {
-    for (let j = 10; j < 500; j += 50) {
-        ctx.strokeRect(i,j,50,50);
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+        ctx.strokeRect(margin + i*cellSize, margin + j*cellSize, cellSize, cellSize);
     }
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    var xPos = e.clientX;
+    var yPos = e.clientY;
+
+    getCell(xPos, yPos);    
+});
+
+function getCell (xPos, yPos) {
+    xPos -= margin;
+    yPos -= margin;
+    
+    xCoord = Math.floor(xPos/cellSize);
+    yCoord = Math.floor(yPos/cellSize);
+
+    console.log(`xCoord = ${xCoord}, yCoord = ${yCoord}`);
 }
